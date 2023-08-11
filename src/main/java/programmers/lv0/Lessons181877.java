23fecf1ee3f7ddb9fdaa4b218b9a0bca5ad490fa
rugii913 <rugii913@gmail.com>
2023-08-11 17:20:08 +0900

    lv0 대문자로 바꾸기

---
+++ src/main/java/programmers/lv0/Lessons181877.java
@@ -0,0 +1,10 @@
+package programmers.lv0;
+
+// lv0 대문자로 바꾸기
+public class Lessons181877 {
+
+    // 풀이 1
+    public String solution1(String myString) {
+        return myString.toUpperCase();
+    }
+}

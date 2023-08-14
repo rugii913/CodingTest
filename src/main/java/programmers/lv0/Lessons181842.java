6135a37f118660ef56b1f736c6cb6607ddf2584c
rugii913 <rugii913@gmail.com>
2023-08-14 16:43:46 +0900

    lv0 부분 문자열  * string.contains(~)

---
+++ src/main/java/programmers/lv0/Lessons181842.java
@@ -0,0 +1,10 @@
+package programmers.lv0;
+
+// lv0 부분 문자열
+public class Lessons181842 {
+
+    // 풀이 1 - contains()
+    public int solution1(String str1, String str2) {
+        return str2.contains(str1) ? 1 : 0;
+    }
+}

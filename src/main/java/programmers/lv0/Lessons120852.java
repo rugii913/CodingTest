d7afec2f0bd4205d0bea41af025cf73cd24af25b
rugii913 <rugii913@gmail.com>
2023-08-17 01:20:35 +0900

    (추가)lv0 소인수 분해 풀이 2 - 정렬 안 한 문제 있으나 테스트는 통과

--- src/main/java/programmers/lv0/Lessons120852.java
+++ src/main/java/programmers/lv0/Lessons120852.java
@@ -4,14 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 // lv0 소인수 분해
-// 120846 isPrime 함수 바꿔야 함...
-/*
-*
-*
-*
-*
-* */
 public class Lessons120852 {
+    /*
+    * https://st-lab.tistory.com/152, https://blog.hyelie.com/entry/%EC%95%BD%EC%88%98-%EC%86%8C%EC%9D%B8%EC%88%98%EB%B6%84%ED%95%B4-%EC%B5%9C%EB%8C%80%EA%B3%B5%EC%95%BD%EC%88%98%EC%B5%9C%EC%86%8C%EA%B3%B5%EB%B0%B0%EC%88%98-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98
+    * - 소인수 분해 알고리즘 관련 위 두 블로그 참고할 것
+    * - 소인수 분해는 원래 brute forced이다. 조사할 대상은 sqrt로 줄일 수 있을 뿐
+    *   - 자연수 n에 대해 n의 어떤 약수 x와 n/x에 대해 min(x, n/x) <= sqrtn이다.
+    * */
 
     // 풀이 1 - 8점...?
     // 1. 약수를 찾는다. 2 ~ n brute force
@@ -60,9 +59,59 @@ public class Lessons120852 {
         }
         return answer;
     }
-    private boolean isPrime(int i) {
-        for (int j = 2; j < i; j++) {
-            if (i % j == 0) {
+
+    // 풀이 2 - 풀이 1에서 약수 배열 구하는 메서드 분리
+    // 단순 배열만 사용해서 빠르다.
+    // ! 이 풀이의 문제점 getDivisor했을 때 정렬되어 있지 않음.
+    //   - 특이한 점은 그래도 테스트 케이스들을 모두 통과한다는 점
+    public int[] solution2(int n) {
+        int[] divisors = getDivisor(n);
+
+        int countPrimeFactor = 0;
+        int[] tmpArray = new int[divisors.length];
+        for (int i = 0; i < divisors.length; i++) {
+            if (isPrime(divisors[i])) {
+                tmpArray[countPrimeFactor++] = divisors[i];
+            }
+        }
+
+        int[] primeFactors = new int[countPrimeFactor];
+        for (int i = 0; i < countPrimeFactor; i++) {
+            primeFactors[i] = tmpArray[i];
+        }
+
+        return primeFactors;
+    }
+    private int[] getDivisor(int number) {
+        int count = 0;
+        int sqrtNumber = (int) Math.sqrt(number);
+        int[] tmpArray = new int[sqrtNumber * 2];
+
+        for (int i = 1; i <= sqrtNumber; i++) {
+            if (number % i == 0) {
+                if (i * i == number) {
+                    tmpArray[count++] = i;
+                } else {
+                    tmpArray[count++] = i;
+                    tmpArray[count++] = number / i;
+                }
+            }
+        }
+
+        int[] divisors = new int[count];
+        for (int j = 0; j < count; j++) {
+            divisors[j] = tmpArray[j];
+        }
+
+        return divisors;
+    }
+    private boolean isPrime(int number) {
+        if (number == 1) {
+            return false;
+        }
+        int sqrtNumber = (int) Math.sqrt(number);
+        for (int i = 2; i <= sqrtNumber; i++) {
+            if (number % i == 0) {
                 return false;
             }
         }
--- src/test/java/programmers/lv0/TmpTest.java
+++ src/test/java/programmers/lv0/TmpTest.java
@@ -16,6 +16,7 @@ class TmpTest {
     void solution() {
         // assertEquals(result, solution.solution1(n));
         // 배열 비교는 assertEquals(~) 사용하면 안 되고 assertArrayEquals() 사용해야 함
-        assertArrayEquals(result, solution.solution1(n));
+        Arrays.stream(solution.solution2(n)).forEach(System.out::println);
+        assertArrayEquals(result, solution.solution2(n));
     }
 }

a6968b11f95f559ecd6ca0e40d21fadc5d2bfcb2
rugii913 <rugii913@gmail.com>
2023-08-26 22:05:31 +0900

    (추가)lv0 등수 매기기 *** ! 풀이4 보조 변수 활용 아이디어 참고

--- src/main/java/programmers/lv0/Lessons120882.java
+++ src/main/java/programmers/lv0/Lessons120882.java
@@ -1,11 +1,17 @@
 package programmers.lv0;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 // lv0 등수 매기기
 public class Lessons120882 {
+    /*
+    * 풀이 4가 풀이 1보다 효율적 - 보조 변수 사용해서 정렬하는 수고가 덜어짐
+    * 조건에 따라 사용 불가능한 경우도 있을 것 같지만 보조변수를 적극 활용해볼 것
+    * */
 
     // 풀이 1(테스트 3,6 관련 질문하기 참고) - 평균이 int가 아니라 double인 경우
     public int[] solution1(int[][] score) {
@@ -40,7 +46,7 @@ public class Lessons120882 {
         return rank;
     }
 
-    // 풀이 2(테스트 3,6 관련 질문하기 참고) - 평균이 int가 아니라 double인 경우
+    // 풀이 2(다른 풀이 참고) - ArrayList 사용
     public int[] solution2(int[][] score) {
         List<Integer> scoreList = new ArrayList<>();
         for(int[] t : score){
@@ -55,8 +61,38 @@ public class Lessons120882 {
         for (int i = 0; i < score.length; i++) {
             answer[i] = scoreList.indexOf(score[i][0] + score[i][1]) + 1;
             // List의 indexOf(object) 메서드를 사용할 수 있으므로 풀이 1처럼 표면적으로 중첩 for문은 안 쓰게 됨
-            // 물론 ArrayList 구현 상 indexOf(object) -> indexOfRange(Object o, int start, int end)는 for문을 사용함 
+            // 물론 ArrayList 구현 상 indexOf(object) -> indexOfRange(Object o, int start, int end)는 for문을 사용함
         }
         return answer;
+    }
+
+    // 풀이 3(다른 풀이 참고) - stream
+    public int[] solution3(int[][] score) {
+        return Arrays.stream(score).map(ints -> Arrays.stream(ints).average().orElse(0))
+                .mapToInt(d -> Arrays.stream(score).map(ints -> Arrays.stream(ints).average().orElse(0)).sorted(Comparator.reverseOrder()).collect(Collectors.toList()).indexOf(d) + 1)
+                .toArray();
+    }
+
+    // 풀이 4(다른 풀이 참고) - count라는 보조 변수를 사용함으로써 정렬하는 수고를 줄임
+    // 같은 점수인 경우 같은 등수가 되는 조건이 있으므로 이런 형태가 가능한 듯
+    public int[] solution4(int[][] score) {
+        // 풀이 원본에서 변수명 변경 map -> avg
+        double[] avg = new double[score.length];
+        for (int i = 0; i < score.length; i++) {
+            avg[i] = (score[i][0] + score[i][1]) / 2.0;
+        }
+
+        int[] result = new int[score.length];
+        for (int i = 0; i < score.length; i++) {
+            int count = 0; // 보조변수
+            for (int j = 0; j < score.length; j++) {
+                if (avg[i] < avg[j]) {
+                    count++;
+                }
+            }
+            result[i] = count + 1;
+        }
+
+        return result;
     }
 }

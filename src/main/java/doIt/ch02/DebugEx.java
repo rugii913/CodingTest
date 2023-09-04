1e60e8d1311c32b5f5aec5ec71342f7c01ade60f
rugii913 <rugii913@gmail.com>
2023-09-04 14:02:24 +0900

    (오류 2 관련 잘못 작성한 부분 있어서 수정)

--- src/main/java/doIt/ch02/DebugEx.java
+++ src/main/java/doIt/ch02/DebugEx.java
@@ -11,13 +11,13 @@ public class DebugEx {
         int A[] = new int[100_001];
         int S[] = new int[100_001];
         // for (int i = 1; i < 10_000; i++) { // 오류 2. 반복문에서 인덱스 범위 지정 오류 찾아보기
-        for (int i = 0; i <= 100_000; i++) { // 반복 범위를 잘못 지정하거나 비교 연산자를 반대로 사용하는 것 유의
+        for (int i = 1; i <= 100_000; i++) { // 반복 범위를 잘못 지정하거나 비교 연산자를 반대로 사용하는 것 유의
             A[i] = (int) (Math.random() * Integer.MAX_VALUE);
             S[i] = S[i - 1] + A[i];
         }
         for (int t = 1; t < testcase; t++) {
+            int query = sc.nextInt(); // query는 합할 범위의 개수라고 생각하면 될 것 같다.
             int answer = 0; // 오류 1. 변수 초기화 오류 찾아보기 - 그 다음 테스트로 넘어갈 때 answer는 초기화되어야 한다.
-            int query = sc.nextInt();
             for (int i = 0; i < query; i++) {
                 int start = sc.nextInt();
                 int end = sc.nextInt();

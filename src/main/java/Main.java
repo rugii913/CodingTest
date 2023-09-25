6c9c5cf54e29638feb9a17f3f71b8b97d58a2c69
rugii913 <rugii913@gmail.com>
2023-09-25 13:59:03 +0900

    lv0 문자열 반복해서 출력하기-string.repeat()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
+import programmers.lv0.Lessons181950;
+
 public class Main {
     public static void main(String[] args) {
-
+        new Lessons181950().solution1();
     }
 }
---
+++ src/main/java/programmers/lv0/Lessons181950.java
@@ -0,0 +1,22 @@
+package programmers.lv0;
+
+import java.util.Scanner;
+
+// lv0 문자열 반복해서 출력하기
+public class Lessons181950 {
+
+    // 풀이 1
+    // - 굳이 StringBuilder로 반복작업 할 필요는 없음, 속도 차이도 없고, IDE에서도 repeat()을 쓰라고 추천함
+    // - string.repeat()은 내부에서 character storage를 위한 byte[]와 System.arraycope(~)를 사용함
+    public void solution1() {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = sc.nextInt();
+
+        /*
+        String repeatStr = str.repeat(n);
+        System.out.print(repeatStr);
+        */
+        System.out.print(str.repeat(n));
+    }
+}

55b81b38bc5a1e63b7404280f04e88a7b22d283f
rugii913 <rugii913@gmail.com>
2023-09-26 11:48:46 +0900

    lv0 덧셈식 출력하기 *format string syntax

---
+++ src/main/java/programmers/lv0/Lessons181947.java
@@ -0,0 +1,17 @@
+package programmers.lv0;
+
+
+import java.util.Scanner;
+
+// lv0 덧셈식 출력하기
+public class Lessons181947 {
+
+    // 풀이 1 - format string syntax
+    public void solution1() {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.printf("%d + %d = %d", a, b, a + b);
+    }
+}

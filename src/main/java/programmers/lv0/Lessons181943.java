8ce7a832c02381f0c257f2ba8c52dd29793337f8
rugii913 <rugii913@gmail.com>
2023-09-27 15:51:13 +0900

    (추가)lv0 문자열 겹쳐 쓰기-new String(char[])vs.String.valueOf(char[])

--- src/main/java/programmers/lv0/Lessons181943.java
+++ src/main/java/programmers/lv0/Lessons181943.java
@@ -6,6 +6,7 @@ public class Lessons181943 {
      * - 풀이 1: getChars(~), String.valueOf(char[])
      * - 풀이 2-1: substring(~), concat(~)
      * - 풀이 3: String의 replace(~)와 StringBuilder의 replace(~)
+     * - 풀이 4-2: String.valueOf(char[])보다 new String(char[])가 더 빠르다?
      */
 
     // 풀이 1 - string.getChars(~), String.valueOf(char[]) 사용 // 0.03 ~ 0.09ms
@@ -32,7 +33,7 @@ public class Lessons181943 {
         return (before.concat(overwrite_string)).concat(after);
     }
 
-    // 풀이 3(다른 풀이 참고) // 0.03 ~ 0.15ms - 풀이 1보다 느리다. 굳이 StringBuilder를 사용할 필요가 없다.
+    // 풀이 3(다른 풀이 참고) // 0.03 ~ 0.15ms - 풀이 1보다 약간 느리다. 굳이 StringBuilder를 사용할 필요가 없다.
     public String solution3(String my_string, String overwrite_string, int s) {
         StringBuilder sb = new StringBuilder(my_string);
         sb.replace(s, s + overwrite_string.length(), overwrite_string);
@@ -43,5 +44,34 @@ public class Lessons181943 {
         // - replace(int start, int end, String str)
 
         return sb.toString();
+    }
+
+    // 풀이 4(다른 풀이 참고) // 0.02 ~ 0.12ms
+    public String solution4(String my_string, String overwrite_string, int s) {
+        char[] charArray1 = my_string.toCharArray();
+        char[] charArray2 = overwrite_string.toCharArray();
+        for (int i = 0; i < overwrite_string.length(); i++) {
+            charArray1[i + s] = charArray2[i];
+        }
+
+        return String.valueOf(charArray1);
+    }
+
+    // 풀이 4-1(다른 풀이 수정) // 0.03 ~ 0.11ms
+    public String solution4_1(String my_string, String overwrite_string, int s) {
+        char[] myStringCharArray = my_string.toCharArray();
+        char[] overwriteStringCharArray = overwrite_string.toCharArray();
+        System.arraycopy(overwriteStringCharArray, 0, myStringCharArray, s, overwrite_string.length());
+
+        return String.valueOf(myStringCharArray);
+    }
+
+    // 풀이 4-2(다른 풀이 수정) // 0.02 ~ 0.08ms - new String(char[])가 더 빠르다?
+    public String solution4_2(String my_string, String overwrite_string, int s) {
+        char[] myStringCharArray = my_string.toCharArray();
+        char[] overwriteStringCharArray = overwrite_string.toCharArray();
+        System.arraycopy(overwriteStringCharArray, 0, myStringCharArray, s, overwrite_string.length());
+
+        return new String(myStringCharArray);
     }
 }

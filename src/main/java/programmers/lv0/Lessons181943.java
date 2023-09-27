53d12ba8007545068e428829c7bb0d54a2b8c6a6
rugii913 <rugii913@gmail.com>
2023-09-27 15:39:48 +0900

    lv0 문자열 겹쳐 쓰기 **getChars(~), String.valueOf(char[]), concat(~)
    ** 기억해둘 메서드들
     - 풀이 1: **getChars(~), **String.valueOf(char[])
     - 풀이 2-1: substring(~), ***concat(~)
     - 풀이 3: String의 replace(~)와 StringBuilder의 replace(~) 비교

---
+++ src/main/java/programmers/lv0/Lessons181943.java
@@ -0,0 +1,47 @@
+package programmers.lv0;
+
+// lv0 문자열 겹쳐 쓰기
+public class Lessons181943 {
+    /*
+     * - 풀이 1: getChars(~), String.valueOf(char[])
+     * - 풀이 2-1: substring(~), concat(~)
+     * - 풀이 3: String의 replace(~)와 StringBuilder의 replace(~)
+     */
+
+    // 풀이 1 - string.getChars(~), String.valueOf(char[]) 사용 // 0.03 ~ 0.09ms
+    public String solution1(String my_string, String overwrite_string, int s) {
+        char[] charsOfAnswer = my_string.toCharArray();
+        overwrite_string.getChars(0, overwrite_string.length(), charsOfAnswer, s);
+
+        return String.valueOf(charsOfAnswer);
+    }
+
+    // 풀이 2(다른 풀이 참고) - substring(~) 사용 // 9.66 ~ 15.17ms
+    public String solution2(String my_string, String overwrite_string, int s) {
+        String before = my_string.substring(0, s);
+        String after = my_string.substring(s + overwrite_string.length());
+        
+        return before + overwrite_string + after;
+    }
+
+    // 풀이 2-1(다른 풀이 수정) - substring(~) + string.concat() // 0.02 ~ 0.04ms 100배 이상 차이, 좀 더 빠르게 하고 싶다면 string.concat(~) 사용
+    public String solution2_1(String my_string, String overwrite_string, int s) {
+        String before = my_string.substring(0, s);
+        String after = my_string.substring(s + overwrite_string.length());
+
+        return (before.concat(overwrite_string)).concat(after);
+    }
+
+    // 풀이 3(다른 풀이 참고) // 0.03 ~ 0.15ms - 풀이 1보다 느리다. 굳이 StringBuilder를 사용할 필요가 없다.
+    public String solution3(String my_string, String overwrite_string, int s) {
+        StringBuilder sb = new StringBuilder(my_string);
+        sb.replace(s, s + overwrite_string.length(), overwrite_string);
+        // cf. String의 replace(~)와 StringBuilder의 replace(~)는 사용 방법이 많이 다르다.
+        // String의 replace(~)
+        // - 1. replace(char oldChar, char newChar) 2. replace(CharSequence target, CharSequence replacement)
+        // StringBuilder의 replace(~)
+        // - replace(int start, int end, String str)
+
+        return sb.toString();
+    }
+}

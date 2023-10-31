42c29abd047ca92a09ef2588d47f688abf43fba6
rugii913 <rugii913@gmail.com>
2023-10-31 16:06:15 +0900

    (주석)ch4-여러가지 정렬 알고리즘 주석

---
+++ src/main/java/doIt/ch04Sort/P2750.java
@@ -0,0 +1,21 @@
+package doIt.ch04Sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+// Do it 코딩테스트 ch4-문제 015 수 정렬하기 1(문제 제목: 수 정렬하기) // P2750
+public class P2750 {
+    /*
+    * ㅁ 여러가지 정렬 알고리즘
+    *   ㅇ 버블(bubble): 데이터의 인접 요소끼리 비교하고, swap 연산을 수행하며 정렬하는 방식
+    *   ㅇ 선택(selection): 대상에서 가장 크거나 작은 데이터를 찾아가 선택을 반복하면서 정렬
+    *   ㅇ 삽입(insertion): 대상을 선택해 정렬된 영역에서 선택 데이터의 적절한 위치를 찾아 삽입하면서 정렬
+    *   ㅇ 퀵(quick): pivot 값을 선정해 해당 값을 기준으로 정렬
+    *   ㅇ 병합(merge): 이미 정렬된 부분 집합들을 효율적으로 병합해 전체를 정렬
+    *   ㅇ 기수(radix): 데이터의 자릿수를 바탕으로 비교해 데이터를 정렬
+    * */
+}
